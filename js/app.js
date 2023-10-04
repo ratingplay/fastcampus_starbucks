08b4a8d16da454dae43a8cdf4de1ce15478fbd41
ratingplay <ratingplay@ratingplay.com>
2023-10-04 17:26:39 +0900

    divide js&css files to common

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInput = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInput.focus();
-})
-
-searchInput.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInput.setAttribute('placeholder', '통합검색');
-})
-searchInput.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInput.setAttribute('placeholder', '');
-})
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -143,5 +127,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
